43643b643e5ef21cd6182fb26060e05853ce3abe
John Terve <89539449797@mail.ru>
2023-02-04 07:57:20 +0300

    Add Query

--- src/main/java/com/example/hw_8_2_1_1_dao_hibernate/repository/PersonRepository.java
+++ src/main/java/com/example/hw_8_2_1_1_dao_hibernate/repository/PersonRepository.java
@@ -2,12 +2,18 @@ package com.example.hw_8_2_1_1_dao_hibernate.repository;
 
 import com.example.hw_8_2_1_1_dao_hibernate.base.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, String> {
-    List<Person> findAllByCity_of_living (String city_of_living);
-    List<Person> findAllByAgeLessThan (int age);
-    List<Optional<Person>> findAllByNameAndSurname (String name, String surname);
+    @Query(value = "select p from Person p where p.city_of_living = :city")
+    List<Person> findAllByCity_of_living (@Param("city") String city_of_living);
+    @Query(value = "select p from Person p where p.age < :age")
+    List<Person> findAllByAgeLessThan (@Param("age") int age);
+    @Query(value = "select p from Person p where p.name = :name and p.surname = :surname")
+    List<Optional<Person>> findAllByNameAndSurname (@Param("name") String name,
+                                                    @Param("surname") String surname);
 }
